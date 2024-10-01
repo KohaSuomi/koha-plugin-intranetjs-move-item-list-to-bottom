3057c30f4dcc2fc6650063b4384cfad5ab0e8648
lmstrand <lmstrand@gmail.com>
2024-10-01 14:47:18 +0300

    Fix scrolling to edit form

--- Koha/Plugin/Fi/KohaSuomi/MoveItemListToBottom/script.js
+++ Koha/Plugin/Fi/KohaSuomi/MoveItemListToBottom/script.js
@@ -1,8 +1,11 @@
 /*Siirretään item-listaus sivun loppuun itemin muokkaussivulla (Tritonia, päivitetty 2020)*/
 $(document).ready(function () {
+    
     $('#cat_additem #cataloguing_additem_itemlist').after($('#cat_additem #cataloguing_additem_itemlist #itemst_wrapper').parents('div').html());
     $('#cat_additem #cataloguing_additem_itemlist #itemst_wrapper').parent('div').hide();
     $('#cat_additem #cataloguing_additem_itemlist').prepend($('#cat_additem #cataloguing_additem_itemlist>.row'));
+    
+    document.getElementById("cataloguing_additem_newitem").scrollIntoView();
 });
 
 /// LOPPU ///
