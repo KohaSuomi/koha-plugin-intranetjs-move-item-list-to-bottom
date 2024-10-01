a9a9a60a8a342ffb29c70ca963371a350e567fc8
lmstrand <lmstrand@gmail.com>
2024-10-01 13:49:44 +0300

    Add script

--- Koha/Plugin/Fi/KohaSuomi/MoveItemListToBottom/script.js
+++ Koha/Plugin/Fi/KohaSuomi/MoveItemListToBottom/script.js
@@ -1,1 +1,8 @@
+/*Siirretään item-listaus sivun loppuun itemin muokkaussivulla (Tritonia, päivitetty 2020)*/
+$(document).ready(function () {
+    $('#cat_additem #cataloguing_additem_itemlist').after($('#cat_additem #cataloguing_additem_itemlist #itemst_wrapper').parents('div').html());
+    $('#cat_additem #cataloguing_additem_itemlist #itemst_wrapper').parent('div').hide();
+    $('#cat_additem #cataloguing_additem_itemlist').prepend($('#cat_additem #cataloguing_additem_itemlist>.row'));
+});
 
+/// LOPPU ///
